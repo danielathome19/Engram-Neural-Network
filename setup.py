42e4debacd4cae21b6efcfc84402e04e63d80bcf
Daniel J. Szelogowski <24834480+danielathome19@users.noreply.github.com>
2025-07-29 17:23:57 -0500

    Added meta files, README, GH actions

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Be sure to update the version number in both "setup.py" and "meta.yaml" files, and the README and CITATION versions.
+setup(
+    name='tensorflow-engram',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Daniel Szelogowski',
+    description='A Python package for adding biologically-inspired Hebbian memory and engram layers to TensorFlow/Keras models, supporting memory traces, plasticity, attention, and sparsity for neural sequence learning.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://danielathome19.github.io/Engram-Neural-Network',
+    project_urls={
+        'Documentation': 'https://danielathome19.github.io/Engram-Neural-Network',
+        'Source': 'https://github.com/danielathome19/Engram-Neural-Network',
+        'Tracker': 'https://github.com/danielathome19/Engram-Neural-Network/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD-3-Clause License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
